c19cdc79c7b30907cc5b8cf4c1882fe492928b09
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-08 19:28:14 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: santh
+ * Date: 5/8/2017
+ * Time: 7:18 PM
+ */
+
+require_once "vendor/autoload.php";
+use Twilio\Twiml;
+
+$response = new Twiml();
+$response->say("Hello World");
+
+header("content-type: text/xml");
+
+echo $response;
+
+?>
